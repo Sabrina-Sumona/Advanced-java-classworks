39300211dc0ae0204b5ae5919fd170d0911d7d88
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-22 23:34:20 +0600

    class1: file input streams & file output streams (updated)

--- src/ReadBytes.java
+++ src/ReadBytes.java
@@ -1,30 +1,20 @@
 import java.io.*;
 
-class ReadBytes
-{
-    public static void main(String[] args)
-    {
+class ReadBytes {
+    public static void main(String[] args) {
         FileInputStream infile;
         int b;
-
-        try
-        {
+        try {
             infile = new FileInputStream("city.txt");
-            while((b = infile.read()) != -1)
-            {
+            while((b = infile.read()) != -1) {
                 System.out.print((char)b);
-                if(b == ',')
-                {
+                if(b == ','){
                     System.out.println();
                 }
             }
             infile.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
-
-        catch(IOException ioe)
-        {
-            System.out.println(ioe);
-        }
-
     }
 }
--- src/WriteBytes.java
+++ src/WriteBytes.java
@@ -1,18 +1,15 @@
 import java.io.*;
 
-class WriteBytes
-{
-    public static void main(String[] args)
-    {
+class WriteBytes {
+    public static void main(String[] args){
         byte[] cities = {'D','H','A','K','A',',','K','H','U','L','N','A',',', 'R','A','J','S','H','A','H','I',',','S','A','V','A','R',',','B','O','G','R','A'};
         FileOutputStream outfile;
         try{
             outfile = new FileOutputStream("city.txt");
             outfile.write(cities);
             outfile.close();
-        }
-        catch(IOException ioe) {
-            ioe.printStackTrace();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
