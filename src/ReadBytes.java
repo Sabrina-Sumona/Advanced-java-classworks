c589be2a98f63ce5ea8eb0a8fc7863dea2b3c6f9
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-22 20:56:49 +0600

    class1: file input streams & file output streams

---
+++ src/ReadBytes.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+class ReadBytes
+{
+    public static void main(String[] args)
+    {
+        FileInputStream infile;
+        int b;
+
+        try
+        {
+            infile = new FileInputStream("city.txt");
+            while((b = infile.read()) != -1)
+            {
+                System.out.print((char)b);
+                if(b == ',')
+                {
+                    System.out.println();
+                }
+            }
+            infile.close();
+        }
+
+        catch(IOException ioe)
+        {
+            System.out.println(ioe);
+        }
+
+    }
+}
